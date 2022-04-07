6d30e32d9e3da1ae578f27e4757dbf77db63f1b0
Billy Ponto <bpgponto@gmail.com>
2022-04-08 02:34:58 +0800

    vue router

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo repellat accusantium minima saepe tenetur assumenda perferendis enim quasi error voluptatem aspernatur itaque, labore delectus, non autem in deleniti? Velit reiciendis amet deleniti aut reprehenderit, ad impedit officia totam accusamus nesciunt, veniam autem fuga tenetur magnam ipsam in obcaecati, minus id. Officia nobis, reprehenderit recusandae modi labore sint. Aperiam, eos ratione? Commodi cum hic, consequuntur iure nihil dolorem minus repellat iusto cupiditate fugiat a fugit deserunt vitae ab atque sequi ullam alias mollitia impedit illo. Pariatur laboriosam modi, sit tempore eos alias neque quos cupiditate totam, quidem voluptatem iusto error nobis.</p>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus repellat harum perferendis exercitationem omnis, quisquam labore eos quasi reiciendis temporibus nihil quidem. Est, deleniti rem! Ea ab, excepturi dolore error quibusdam id delectus accusantium repudiandae magnam alias quod quasi. Deserunt magnam vel aspernatur at possimus hic fuga vitae, modi aut assumenda maxime labore mollitia nam officia quas ab deleniti. Amet sunt non dicta ad, odio animi, corrupti beatae est reiciendis sequi vel hic quia molestias repellendus repellat exercitationem quisquam tenetur, distinctio sit expedita. Quis modi sequi non error. Omnis, molestiae minima! Nobis optio facilis exercitationem necessitatibus, cupiditate voluptatem similique atque.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus repellat harum perferendis exercitationem omnis, quisquam labore eos quasi reiciendis temporibus nihil quidem. Est, deleniti rem! Ea ab, excepturi dolore error quibusdam id delectus accusantium repudiandae magnam alias quod quasi. Deserunt magnam vel aspernatur at possimus hic fuga vitae, modi aut assumenda maxime labore mollitia nam officia quas ab deleniti. Amet sunt non dicta ad, odio animi, corrupti beatae est reiciendis sequi vel hic quia molestias repellendus repellat exercitationem quisquam tenetur, distinctio sit expedita. Quis modi sequi non error. Omnis, molestiae minima! Nobis optio facilis exercitationem necessitatibus, cupiditate voluptatem similique atque.</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 }
 </script>
