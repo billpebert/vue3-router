e7fc3edd3e1ffeb464f476a0bdac0148adbe5de0
Billy Ponto <bpgponto@gmail.com>
2022-04-08 11:08:22 +0800

    fetching and loading data with local json-server

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,21 +1,31 @@
 <template>
-  <h1>Job Details Page</h1>
-  <p>The job id is {{ id }}</p>
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <p>The job id is {{ id }}</p>
+    <p>{{ job.details }}</p>
+  </div>
+  <div v-else>
+    <p>Loading data...</p>
+  </div>
 </template>
 
 <script>
-export default {
-
-    props: ['id']
-    // data(){
-    //     return{
-    //         id: this.$route.params.id
-    //     }
-    // }
-
-}
+	export default {
+		props: ["id"],
+		data(){
+		    return{
+          job: null //null is the same as false value
+         //job: []
+        }
+		},
+		mounted() {
+			fetch("http://localhost:3000/jobs/" + this.id)
+				.then((res) => res.json())
+				.then((data) => (this.job = data))
+				.catch((err) => console.log(err.message));
+		},
+	};
 </script>
 
 <style>
-
 </style>
